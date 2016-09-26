3f9f7598d0982d155dc75cf402284c333f463125
hsenidVidushka <vidushka@hsenidlanka.com>
2016-09-25 23:27:06 -0500

    @nd commit

---
+++ src/main/java/com/hsenid/flow_control_client/flow_controller_client.java
@@ -0,0 +1,12 @@
+package com.hsenid.flow_control_client;
+
+import com.hsenid.flow_controll.flowControll;
+
+/**
+ * Created by Vidushka on 9/21/16.
+ */
+public class flow_controller_client {
+    public static void main(String[] args) {
+        flowControll.switch_case(21);
+    }
+}
